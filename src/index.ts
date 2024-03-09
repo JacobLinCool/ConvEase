d4f7055b18c4c655f919870a3b6a7cca828b589c
JacobLinCool <jacoblincool@gmail.com>
2024-03-10 02:11:54 +0800

    feat: graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,12 @@ import { bot } from "./bot";
 import { config } from "./config";
 
 bot.login(config.BOT_TOKEN);
+
+const SIGNALS = ["SIGINT", "SIGTERM", "SIGUSR2"];
+for (const signal of SIGNALS) {
+	process.once(signal, async () => {
+		await bot.destroy();
+		console.log(`Signal ${signal} received. Shutting down...`);
+		process.exit(0);
+	});
+}
